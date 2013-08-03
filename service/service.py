205493873e9bdb551aba7023a2fc55c334ce464f
Gabriel Harp <gabrielharp@gmail.com>
2013-08-02 21:52:39 -0700

    service pying

--- service/service.py
+++ service/service.py
@@ -25,7 +25,7 @@ from py2neo import neo4j, cypher
 
 # Attach to the graph db instance
 #set below in main to:
-graph_db = neo4j.GraphDatabaseService("http://40cff9255.hosted.neo4j.org:7391")
+graph_db = neo4j.GraphDatabaseService("http://40cff9255.hosted.neo4j.org:7391/db/data/")
 
 #graph_db=None
 
@@ -344,7 +344,7 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-    graph_db = neo4j.GraphDatabaseService("http://40cff9255.hosted.neo4j.org:7391")
+    graph_db = neo4j.GraphDatabaseService("http://40cff9255.hosted.neo4j.org:7391/db/data/")
     print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
     try:
         httpd.serve_forever()
